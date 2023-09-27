80ab21610ef5ed70334e911da218087e393db350
Joamthai <joamthai@gmail.com>
2023-09-27 10:26:08 +0700

    finish:setError

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,51 @@
+import { useState } from 'react';
+import Joi from 'Joi';
+
+const schema = Joi.object({
+  username: Joi.string().min(3).max(30).required(),
+  password: Joi.string().min(6).alphanum().required(),
+  confirmPassword: Joi.string().valid(Joi.ref('password')).required(),
+});
+
 export default function RegisterPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [error, setError] = useState({
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleSubmitForm = (event) => {
+    event.preventDefault();
+    const { value, error } = schema.validate(
+      {
+        username,
+        password,
+        confirmPassword,
+      },
+      { abortEarly: false }
+    );
+    if (error) {
+      const nextError = {
+        username: '',
+        password: '',
+        confirmPassword: '',
+      };
+      for (let item of error.details) {
+        nextError[item.path[0]] = item.message;
+      }
+      return setError(nextError);
+    }
+  };
+
   return (
-    <section className="flex flex-col gap-4 bg-white p-4 rounded-md">
+    <form
+      className="flex flex-col gap-4 bg-white p-4 rounded-md"
+      onSubmit={handleSubmitForm}
+    >
       <div>
         <label htmlFor="" className="block mb-1 font-semibold">
           Username
@@ -8,6 +53,8 @@ export default function RegisterPage() {
         <input
           type="text"
           className="w-full border bg-gray-100 outline-none px-3 py-1.5 rounded-md focus:ring-2 focus:ring-red-200"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
         />
       </div>
       <div>
@@ -17,6 +64,8 @@ export default function RegisterPage() {
         <input
           type="text"
           className="w-full border bg-gray-100 outline-none px-3 py-1.5 rounded-md focus:ring-2 focus:ring-red-200"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </div>
       <div>
@@ -26,11 +75,13 @@ export default function RegisterPage() {
         <input
           type="text"
           className="w-full border bg-gray-100 outline-none px-3 py-1.5 rounded-md focus:ring-2 focus:ring-red-200"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
         />
       </div>
       <button className="bg-red-400 px-3 py-1.5 rounded-md font-semibold">
         Sign Up
       </button>
-    </section>
+    </form>
   );
 }
